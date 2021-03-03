ff24f620f01a291d46f8147b48f4ebb589d4e263
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-03-03 13:48:16 -0300

    feature-5: implemented absolute imports and integrated react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Root from './pages/Root';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Root />,
   document.getElementById('root')
 );
 
