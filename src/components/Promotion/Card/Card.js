750ff87f82363d838c7d2330286a475a731214c4
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-03-11 23:16:20 -0300

    feature-3: implemented a Modal with createPortal

--- src/components/Promotion/Card/Card.js
+++ src/components/Promotion/Card/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Card.css'
 
-const PromotionCard = ({ promotion }) =>(
+const PromotionCard = ({ promotion, onClickComments }) =>(
 
     <div className='promotion-card'>
       <img src={promotion.imageUrl} alt={promotion.title} className='promotion-card__image'/>
@@ -12,9 +12,14 @@ const PromotionCard = ({ promotion }) =>(
         <footer className='promotion-card__footer'>{promotion.comments.length > 0 && (
           <div className='promotion-card__comment'>"{promotion.comments[0].comment}"</div>
         )}
-          <div className='promotion-card__comments-count'>{promotion.comments.length} {promotion.comments.length > 1 ? 'Comentários': 'Comentário'} </div>
-          <a href={promotion.url} target="_blank" rel="noopener noreferrer" className='promotion-card__link'>IR PARA O SITE</a>
-          <Link to={`/edit/${promotion.id}`}>Editar</Link>
+          <button className='promotion-card__comments-count' onClick={onClickComments}>
+            {promotion.comments.length} 
+            {promotion.comments.length > 1 ? ' Comentários': ' Comentário'} 
+          </button>
+          <a href={promotion.url} 
+            target="_blank" rel="noopener noreferrer" 
+            className='promotion-card__link'>Ir Para o Site</a>
+          <Link to={`/edit/${promotion.id}`} className="promotion-card__edit-button">Editar</Link>
           </footer>
       </div>
     </div>
--- src/components/Promotion/List/List.js
+++ src/components/Promotion/List/List.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PromotionCard from '../Card/Card'
+import UIModal from 'components/UI/Modal/Modal'
 import './List.css'
 
 const PromotionList = ({ loading, promotions, error }) => {
+  const [promotionId, setPromotionId] = useState(null)
   if (error) {
     return <div>Erro Encontrado.</div>
   }
@@ -18,8 +20,14 @@ const PromotionList = ({ loading, promotions, error }) => {
   return (
     <div className="promotion-list">
       {promotions.map((promotion) => (
-        <PromotionCard promotion={promotion} />
+        <PromotionCard
+          promotion={promotion}
+          onClickComments={() => setPromotionId(promotion.id)}
+        />
       ))}
+      <UIModal isOpen={Boolean(promotionId)} onClickClose={() => setPromotionId(null)}>
+        <h1>Comentários</h1>
+      </UIModal>
     </div>
   )
 }
