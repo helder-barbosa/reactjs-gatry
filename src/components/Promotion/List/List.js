d553c13acbf48b087749c0abff5d4b29b78c4108
Helder Barbosa <helderbarbosa.dev@gmail.com>
2021-03-12 22:33:31 -0300

    feature-3-2: Save and List comments in a Modal

--- src/components/Promotion/List/List.js
+++ src/components/Promotion/List/List.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import PromotionCard from '../Card/Card'
-import UIModal from 'components/UI/Modal/Modal'
+import PromotionModal from '../Modal/Modal'
 import './List.css'
 
 const PromotionList = ({ loading, promotions, error }) => {
@@ -25,9 +25,13 @@ const PromotionList = ({ loading, promotions, error }) => {
           onClickComments={() => setPromotionId(promotion.id)}
         />
       ))}
-      <UIModal isOpen={Boolean(promotionId)} onClickClose={() => setPromotionId(null)}>
-        <h1>Comentários</h1>
-      </UIModal>
+      {promotionId && (
+        <PromotionModal
+          promotionId={promotionId}
+          onClickClose={() => setPromotionId(null)}
+        />
+      )}
+
     </div>
   )
 }
